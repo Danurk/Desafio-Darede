ed075ed1cadfa429525c87e9ed4799ff0088e5a4
Carlos Alexandre <40705028+Danurk@users.noreply.github.com>
2022-12-14 14:13:21 -0300

    Update security.py

--- Python/security.py
+++ Python/security.py
@@ -15,11 +15,9 @@ for i in response['SecurityGroups']:
             print("PORT: "+str(j['FromPort']))
             for k in j['IpRanges']:
                 print("IP Ranges: "+k['CidrIp'])
+            if k == {'CidrIp': '0.0.0.0/0'}:
+                response = client.publish(TopicArn='arn:aws:sns:us-east-1:551940803425:Python',Message="O seguinte grupo tem portas abertas para o mundo: "+c['GroupId'])
+                print(response)
         except Exception:
             print("No value for ports and ip ranges available for this security group")
             continue
-        
-print(k)
-if k == {'CidrIp': '0.0.0.0/0'}:
-    response = client.publish(TopicArn='arn:aws:sns:us-east-1:551940803425:Python',Message="O seguinte grupo tem portas abertas para o mundo: "+c['GroupId'])
-    print(response)
